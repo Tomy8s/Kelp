0eabf5dc2aa69e7abe85221aefa8b8a9ff421a85
Tomy8s <tomy8s@hotmail.com>
2016-11-17 10:29:14 +0000

    added unit test for one review per user per restaurant

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -6,4 +6,11 @@ RSpec.describe Review, type: :model do
     expect(review).to have(1).error_on(:rating)
     expect(review).not_to be_valid
   end
+
+  it 'is invalid if user has already reviewed this restaurant' do
+    # User.create()
+    # Restaurant.create()
+    Review.create(restaurant_id: 1, user_id: 1, comments: 'good', rating: 3)
+    review = Review.create(restaurant_id: 1, user_id: 1, comments: 'bad', rating: 0)
+  end
 end
