b6dd7a0cba31f55b12236eef16053486863cd01c
Tomy8s <tomy8s@hotmail.com>
2016-11-18 11:12:50 +0000

    added test for one review per user per restaurant

--- spec/features/reviews_feature_spec.rb
+++ spec/features/reviews_feature_spec.rb
@@ -10,7 +10,7 @@ feature 'reviewing' do
     fill_in "Thoughts", with: 'so good'
     select '5', from: 'Rating'
     click_button 'Leave Review'
-    expect(page).to have_content('so good')
+    expect(page).to have_content 'so good'
   end
 
   scenario  'users must be signed in to leave a review' do
@@ -18,5 +18,11 @@ feature 'reviewing' do
     click_link 'Nandos'
     click_link 'Review Nandos'
     expect(page).to have_content 'You need to sign in or sign up before continuing.'
+  end
+
+  scenario 'user can\'t leave more than one review for the same restaurant' do
+    add_restaurant
+    2.times{ leave_review }
+    expect(page).to have_content 'Error: You have already reviewed A Restaurant'
   end
 end
--- spec/web_helper.rb
+++ spec/web_helper.rb
@@ -6,3 +6,20 @@ def sign_in
   fill_in "Password", with: "password"
   click_button "Log in"
 end
+
+def add_restaurant
+  sign_in
+  click_link 'Add a restaurant'
+  fill_in 'Name', with: 'A Restaurant'
+  fill_in 'Description', with: 'A place to eat'
+  click_button 'Create Restaurant'
+end
+
+def leave_review
+  visit '/'
+  click_link 'A Restaurant'
+  click_link 'Review A Restaurant'
+  fill_in 'Thoughts', with: 'Me like'
+  select 5, from: 'Rating'
+  click_button 'Leave Review'
+end
